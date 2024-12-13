78ddad1db7a32faca72c726d4b9fffa462981c77
Kristen Garnier <kristen@oktopod.io>
2024-12-13 12:31:07 +0100

    Chore : add a comment context

--- src/const/env.ts
+++ src/const/env.ts
@@ -1,6 +1,6 @@
 import crypto from 'node:crypto'
 
+// Par simplicité ici les clefs sont générés au lancement, le mieux serait de mettre en place une rotation régulière des clefs
 export const SESSION_KEY = crypto.randomBytes(32).toString('hex');
 export const HMAC_KEY = crypto.randomBytes(32).toString('hex');
-export const USER_TOKEN_KEY = crypto.randomBytes(32).toString('hex');
 export const COOKIE_KEY = crypto.randomBytes(32).toString('hex');
